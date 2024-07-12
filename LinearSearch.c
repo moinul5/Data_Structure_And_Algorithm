838bbb93800b461a3d0ff34816c44abce5ffc65a
Moinu <mi047776@gmail.com>
2024-07-12 18:41:35 +0600

    Linear Sort

---
+++ LinearSearch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int  main()
+{
+    int arr[5] = {1,2,3,4,5};
+    int k=0;
+
+    for(int i=0; i<5; i++){
+        if(arr[i]==55){
+            printf("found");
+            k++;
+            break;
+        }
+    }
+
+    if(K==0){
+        printf("not found");
+    }
+}
