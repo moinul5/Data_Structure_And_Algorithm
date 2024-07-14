3090de8fc58f16b10cf846dbf9774de611dca5a4
Moinu <mi047776@gmail.com>
2024-07-14 18:34:08 +0600

    Inserting node in the linked list calling a function

---
+++ Single Linked List/InsetingToEnd_SingleLinkedList.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node* addToEnd(struct node *ptr , int data)
+{
+    struct node *temp = malloc(sizeof(struct node));
+    temp -> data = data;
+    temp -> link = NULL;
+    ptr-> link = temp;
+    return temp;
+}
+
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    head -> data = 45;
+    head -> link =NULL;
+
+    struct node *ptr  = head;
+
+    ptr = addToEnd(ptr,34);
+    ptr = addToEnd(ptr,38);
+    ptr = addToEnd(ptr,555);
+    ptr = addToEnd(ptr,3832);
+    ptr = addToEnd(ptr,3814);
+    ptr = addToEnd(ptr,38352678);
+    ptr = addToEnd(ptr,38231);
+
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ",ptr -> data);
+        ptr = ptr-> link;
+    }
+}
